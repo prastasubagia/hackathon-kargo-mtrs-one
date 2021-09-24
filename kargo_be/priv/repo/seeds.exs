fbd3b3fcf010a8747f46dbeb68913bac041c8d05
agungmulyadi <agung.mulyadi@gmail.com>
2021-09-24 14:05:09 +0700

    seeder shipment status

--- kargo_be/priv/repo/seeds.exs
+++ kargo_be/priv/repo/seeds.exs
@@ -9,3 +9,30 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias KargoBe.Repo
+alias KargoBe.Shipments.ShipmentStatus
+
+Repo.insert!(%ShipmentStatus{
+    name: "Created"
+  })
+  Repo.insert!(%ShipmentStatus{
+    name: "Assigned"
+  })
+  Repo.insert!(%ShipmentStatus{
+    name: "Allocated"
+  })
+  Repo.insert!(%ShipmentStatus{
+    name: "Ongoing to Origin"
+  })
+  Repo.insert!(%ShipmentStatus{
+    name: "At Origin"
+  })
+  Repo.insert!(%ShipmentStatus{
+    name: "Ongoing to Destination"
+  })
+  Repo.insert!(%ShipmentStatus{
+    name: "At Destination"
+  })
+  Repo.insert!(%ShipmentStatus{
+    name: "Completed"
+  })
