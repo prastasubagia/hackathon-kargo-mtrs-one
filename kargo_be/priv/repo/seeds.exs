2c8f4ce67ba662335d5de1e7ee2b622dcd74091f
Muhammad Fajar <muhammad.fajar@mitrais.com>
2021-09-24 15:07:27 +0800

    Fix Truck Schema

--- kargo_be/priv/repo/seeds.exs
+++ kargo_be/priv/repo/seeds.exs
@@ -9,3 +9,41 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias KargoBe.Repo
+alias KargoBe.Trucks.TruckType
+alias KargoBe.Trucks.Truck
+
+# Truck Types
+Repo.insert!(%TruckType{
+  name: "FUSO"
+});
+Repo.insert!(%TruckType{
+  name: "PICKUP"
+});
+Repo.insert!(%TruckType{
+  name: "TRONTON"
+});
+Repo.insert!(%TruckType{
+  name: "VAN"
+});
+
+# Trucks
+Repo.insert!(%Truck{
+  license_number: "B1000ABA",
+  license_type: "yellow",
+  production_year: 2007,
+  status: true,
+  stnk_path: "",
+  kir_path: "",
+  truck_type_id: 1
+})
+Repo.insert!(%Truck{
+  license_number: "B2000BAB",
+  license_type: "yellow",
+  production_year: 2010,
+  status: true,
+  stnk_path: "",
+  kir_path: "",
+  truck_type_id: 2
+})
