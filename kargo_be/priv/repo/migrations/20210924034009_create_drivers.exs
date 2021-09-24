ce0ad51e9ee33cf83b928456942807abdf9f826e
prasta subagia <prasta.subagia@gmail.com>
2021-09-24 10:49:01 +0700

    add drivers and static host

---
+++ kargo_be/priv/repo/migrations/20210924034009_create_drivers.exs
@@ -0,0 +1,15 @@
+defmodule KargoBe.Repo.Migrations.CreateDrivers do
+  use Ecto.Migration
+
+  def change do
+    create table(:drivers) do
+      add :driver_name, :string
+      add :phone_number, :string
+      add :id_card, :string
+      add :driver_license, :string
+
+      timestamps()
+    end
+
+  end
+end
